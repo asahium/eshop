1eebe1d640056ad75816e368bd3d02de6e657379
lysanyikkseniia <kdlysanyuk@edu.hse>
2023-07-23 10:02:56 +0300

    Revert "shop added"
    This reverts commit 0ed68f0d167ea36ea04bd2d59b82dc6c7157571e.

--- skinglow/config/config.php
+++
@@ -1,9 +0,0 @@
-<?php
-define('ROOT_URL', 'http://localhost:8888/skinglow/'); //add your localhost link
-// Add your localhost link to ROOT page "Home page"
-
-define('DB_HOST', 'localhost');
-define('DB_USER', 'root');// Add your name in phpMyAdmin
-define('DB_PASS', 'root');  // Add your password in phpMyAdmin
-define('DB_NAME', 'SkinGlowDB'); // Add Database name in phpMyAdmin
-?>
